d5e0078a5a21ec7817e6d1b61e35f5fba3448cda
Jiseok CHOI <jiseok.dev@gmail.com>
2025-05-20 21:41:26 +0900

    feat: add node binding (#5)

---
+++ node/__test__/index.spec.mjs
@@ -0,0 +1,32 @@
+import test from 'ava'
+
+import native from '../index.js'
+import { tmpdir } from 'os'
+import { join } from 'path'
+import { randomBytes } from 'crypto'
+import { unlinkSync } from 'fs'
+
+const { CdbWriter, Cdb } = native
+
+test('CdbWriter/Cdb native roundtrip', (t) => {
+  const dbPath = join(tmpdir(), 'test-cdb-' + randomBytes(8).toString('hex') + '.cdb')
+  const writer = new CdbWriter(dbPath)
+  writer.put(Buffer.from('foo'), Buffer.from('bar'))
+  writer.put(Buffer.from('baz'), Buffer.from('qux'))
+  writer.finalize()
+
+  const cdb = Cdb.open(dbPath)
+  t.deepEqual(cdb.get(Buffer.from('foo')), Buffer.from('bar'))
+  t.deepEqual(cdb.get(Buffer.from('baz')), Buffer.from('qux'))
+  t.is(cdb.get(Buffer.from('notfound')), null)
+
+  const all = cdb.iter()
+  const keys = all.map(e => Buffer.from(e.key).toString())
+  const values = all.map(e => Buffer.from(e.value).toString())
+  t.true(keys.includes('foo'))
+  t.true(keys.includes('baz'))
+  t.true(values.includes('bar'))
+  t.true(values.includes('qux'))
+
+  unlinkSync(dbPath)
+})
